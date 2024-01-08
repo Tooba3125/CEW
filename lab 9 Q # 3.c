1f3723f1a73972b5496e2f731ba0e10b7ae95ac9
Tooba Aftab <142301970+Tooba3125@users.noreply.github.com>
2024-01-08 23:02:13 +0500

    Create lab 9 Q # 3.c

---
+++ lab 9 Q # 3.c
@@ -0,0 +1,4 @@
+// 1.	cpio: Creates or extracts archives. Commonly used with pipes to copy files in and out of cpio or tar archives.
+// 2.	sort: Sorts lines of text files. Useful for organizing data alphabetically or numerically. Can be applied to files or command outputs.
+// 3.	fuser: Displays process IDs using a specified file, file system, or socket. Can also be used to kill processes accessing a particular resource.
+// 4.	file: Determines the type of a file by examining its content. Outputs information about the file format or type, helping identify file characteristics
