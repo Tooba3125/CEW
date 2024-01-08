fbaafb5bac09a88871ed36ecfe911956a3844c08
Tooba Aftab <142301970+Tooba3125@users.noreply.github.com>
2024-01-08 23:18:45 +0500

    Create lab 10 Q # 3.c

---
+++ lab 10 Q # 3.c
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+# Prompt user for input
+echo "Enter a number (1-3):" read number
+
+# Use case statement to display month name case $number in
+1)
+ 
+
+
+
+2)
+
+
+
+3)
+
+
+
+*)
+
+
+
+esac
+ 
+echo "January"
+;;
+
+
+echo "February"
+;;
+
+
+echo "March"
+;;
+
+
+echo "Invalid input. Please enter a number between 1 and 3."
+;;
