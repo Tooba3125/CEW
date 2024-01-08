89fb2c5e76c3e7b1df74f98eea560dbdb559dd26
Tooba Aftab <142301970+Tooba3125@users.noreply.github.com>
2024-01-08 23:03:46 +0500

    Create lab 8 Q # 5.c

---
+++ lab 8 Q # 5.c
@@ -0,0 +1,15 @@
+-	cp Command:
+-	Purpose: Copy files and directories.
+-	Usage:`cp [options] source destination`.
+-	Key Options: `-r` (recursion), `-i` (prompt), `-u` (update).
+
+-	cpio Command:
+-	Purpose: Create or extract archives.
+-	Usage: `cpio [options]`.
+-	Key Options: `-o` (create), `-i` (extract), `-t` (list).
+
+Key Difference:
+-	“cp” is for copying files and directories.
+ 
+-	“cpio” is for creating or extracting archives and is often used in conjunction with other commands in pipelines.
+
