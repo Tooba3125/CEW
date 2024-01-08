ef6f4ae184ae08c35a0760b3bf4a1794222ad7c1
Tooba Aftab <142301970+Tooba3125@users.noreply.github.com>
2024-01-08 23:17:30 +0500

    Create lab 10 Q # 1.c

---
+++ lab 10 Q # 1.c
@@ -0,0 +1,5 @@
+// To test if it’s a directory:
+test -d /usr/bin && echo "It's a directory" || echo "It's not a directory
+
+// To test if it’s a file:
+test -f /usr/bin && echo "It's a file" || echo "It's not a file"
